dbc630eb9e24788fc52a7dcc41063c964a401b8f
Matheus Leal <matheusleal1301@gmail.com>
2024-07-10 15:22:58 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,43 @@
+package chess;
+
+import boardgame.Position;
+
+public class ChessPosition {
+
+	private char column;
+	private int row;
+	
+	
+	
+	public ChessPosition(char column, int row) {
+		if(column < 'a' && column > 'h' && row < 1 && row > 8) {
+			throw new ChessException("Error instantiang ChessPosition. Valid values are from a1 to h8");
+		}
+		this.column = column;
+		this.row = row;
+	}
+
+	public char getColumn() {
+		return column;
+	}
+	
+	public int getRow() {
+		return row;
+	}
+	
+	// Operações com Unicodes
+	protected Position toPosition() {
+		return new Position(8 - row, column - 'a');
+	}
+	
+	// UML - Sublinhado
+	protected static ChessPosition fromPosition(Position position) {
+		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+	}
+	
+	@Override
+	public String toString() {
+		// "" força o compilador entender que o column + row é uma concatenação de strings
+		return "" + column + row;
+	}
+}

f97d439f998ab7dabc0ad2d2065762f90403cd55
Matheus Leal <matheusleal1301@gmail.com>
2024-07-12 10:18:24 -0300

    Moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,10 @@ package application;
 
 import boardgame.Board;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Program {
 
@@ -9,8 +13,23 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
+
+		while (true) { // repetir indefinidamente
+			UI.printBoard(chessMatch.getPieces());
+
+			System.out.println();
+			System.out.print("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
 	}
 
 }
